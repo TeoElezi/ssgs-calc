7405e5fb8bd98904d97682a8d193fb95ff61ed76
TeoElezi <5283664@studenti.unige.it>
2025-05-16 15:47:18 +0200

    Error Fix

--- math.test.js
+++ math.test.js
@@ -1,21 +1,21 @@
-const sum = require('./math').sum;
-test ('adds 1 + 2 to equal 3', () => {
+const { sum, subtract, multiply, divide, pow } = require('./math');
+
+test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-const subtract = require('./math').subtract;
-test ('subtracts 5 - 2 to equal 3', () => {
+test('subtracts 5 - 2 to equal 3', () => {
     expect(subtract(5, 2)).toBe(3);
 });
-const multiply = require('./math').multiply;
-test ('multiplies 3 * 2 to equal 6', () => {
+
+test('multiplies 3 * 2 to equal 6', () => {
     expect(multiply(3, 2)).toBe(6);
 });
-const divide = require('./math').divide;
-test ('divides 6 / 2 to equal 3', () => {
+
+test('divides 6 / 2 to equal 3', () => {
     expect(divide(6, 2)).toBe(3);
 });
-const pow = require('./math').pow;
-test ('raises 2 to the power of 3 to equal 8', () => {
+
+test('raises 2 to the power of 3 to equal 8', () => {
     expect(pow(2, 3)).toBe(8);
 });
